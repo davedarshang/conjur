45fe0f5df159d18bec78a4e5044f88a3eeb73335
Moti Cohen <moti.cohen@cyberark.com>
2019-03-14 11:55:35 +0200

    Decoding and verifying ID Token

--- app/domain/authentication/authn_oidc/authenticate.rb
+++ app/domain/authentication/authn_oidc/authenticate.rb
@@ -3,15 +3,17 @@ require 'command_class'
 module Authentication
   module AuthnOidc
     Authenticate = CommandClass.new(
-      dependencies: {
-        enabled_authenticators: ENV['CONJUR_AUTHENTICATORS'],
-        get_oidc_client_configuration: AuthnOidc::GetOidcClientConfiguration.new,
-        token_factory:          OidcTokenFactory.new,
-        validate_security:      ::Authentication::ValidateSecurity.new,
-        validate_origin:        ::Authentication::ValidateOrigin.new,
-        audit_event:            ::Authentication::AuditEvent.new
-      },
-      inputs:       %i(authenticator_input)
+        dependencies: {
+            enabled_authenticators: ENV['CONJUR_AUTHENTICATORS'],
+            get_oidc_client_configuration: AuthnOidc::GetOidcClientConfiguration.new,
+            token_factory: OidcTokenFactory.new,
+            validate_security: ::Authentication::ValidateSecurity.new,
+            validate_origin: ::Authentication::ValidateOrigin.new,
+            audit_event: ::Authentication::AuditEvent.new,
+            decode_and_verify_id_token: ::Authentication::AuthnOidc::DecodeAndVerifyIdToken.new
+
+        },
+        inputs: %i(authenticator_input)
     ) do
 
       def call
@@ -24,20 +26,14 @@ module Authentication
         request_body = AuthnOidc::AuthenticateRequestBody.new(input.request)
 
         oidc_client_configuration = @get_oidc_client_configuration.(
-          redirect_uri: nil, # not needed for this request
-            service_id: input.service_id,
-            conjur_account: input.account
+            redirect_uri: nil, # not needed for this request
+                service_id: input.service_id,
+                conjur_account: input.account
         )
 
-        # Prepare ID token introspect request
+        id_token_attribs = @decode_and_verify_id_token.(oidc_client_configuration.provider_uri, request_body.id_token)
 
-        # send id token to OIDC Provider
-
-        # Get JSON from OIDC Provider
-
-        # Validate ID Token is active
-
-        input = input.update(username: conjur_username(request_body.id_token, oidc_client_configuration.id_token_user_property))
+        input = input.update(username: conjur_username(id_token_attribs, oidc_client_configuration.id_token_user_property))
 
         @validate_security.(input: input, enabled_authenticators: @enabled_authenticators)
 
@@ -53,13 +49,13 @@ module Authentication
 
       def new_token(input)
         @token_factory.signed_token(
-          account:  input.account,
-          username: input.username
+            account: input.account,
+            username: input.username
         )
       end
 
-      def conjur_username(id_token, id_token_username_field)
-        conjur_username = id_token[id_token_username_field]
+      def conjur_username(id_token_attribs, id_token_username_field)
+        conjur_username = id_token_attribs[id_token_username_field]
         raise IdTokenFieldNotFound, id_token_username_field unless conjur_username.present?
 
         conjur_username
